6626f7347d4cf551ffeec71026b3a5f2c656aa59
Robert McMenemy <total.trance.network@gmail.com>
2022-12-27 18:20:51 +0000

    Fetch file cached function refactor for efficiency

--- point_e/models/download.py
+++ point_e/models/download.py
@@ -8,6 +8,7 @@ from typing import Dict, Optional
 
 import requests
 import torch
+import shutil
 from filelock import FileLock
 from tqdm.auto import tqdm
 
@@ -39,26 +40,41 @@ def fetch_file_cached(
     """
     if cache_dir is None:
         cache_dir = default_cache_dir()
+    
     os.makedirs(cache_dir, exist_ok=True)
     local_path = os.path.join(cache_dir, url.split("/")[-1])
+
+    # If local path exists return it
     if os.path.exists(local_path):
         return local_path
 
+    # Download the file using the requests library
     response = requests.get(url, stream=True)
     size = int(response.headers.get("content-length", "0"))
-    with FileLock(local_path + ".lock"):
-        if progress:
-            pbar = tqdm(total=size, unit="iB", unit_scale=True)
-        tmp_path = local_path + ".tmp"
-        with open(tmp_path, "wb") as f:
-            for chunk in response.iter_content(chunk_size):
-                if progress:
-                    pbar.update(len(chunk))
-                f.write(chunk)
-        os.rename(tmp_path, local_path)
-        if progress:
-            pbar.close()
-        return local_path
+
+    # Use a progress bar if desired
+    if progress:
+        pbar = tqdm(total=size, unit="iB", unit_scale=True)
+
+    # Create a temporary file and download the file in chunks
+    tmp_path = local_path + ".tmp"
+    with open(tmp_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            if progress:
+                pbar.update(len(chunk))
+            f.write(chunk)
+
+    # Use shutil to copy the file from the temporary location to the final destination
+    shutil.copyfile(tmp_path, local_path)
+
+    # Clean up the temporary file
+    os.remove(tmp_path)
+
+    # Hide progress bar
+    if progress:
+        pbar.close()
+
+    return local_path
 
 
 def load_checkpoint(
